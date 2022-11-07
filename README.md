317e9ed07842f5e3fac4750519f72beb9f6fc1d8
Claud Pod <claudiapollinger.cp@gmail.com>
2022-11-07 08:52:30 -0700

    basic outline to application

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_gui_application
+
---
+++ python_gui_application/app.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import filedialog, text
+import os 
+
+root = tk.Tk()
+
+canvas.tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+frame = tk.Frame (root, bg ="white")
+frame.place(relwidth=0.8, relheight)
+
+root.mainloop()
+
